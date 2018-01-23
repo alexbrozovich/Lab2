19316fcbff85b98c54f2bd27fdda30c756a27f5e
alexbrozovich <alexbrozovich@gmail.com>
2018-01-23 16:36:24 -0500

    Taught the program some manners. Now it asks for the user's name and refers to them by it.

--- src/Lab2.java
+++ src/Lab2.java
@@ -5,16 +5,21 @@ public class Lab2 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
-		// set up giant loop
+		//ask for the user's name and use that to make the program friendlier throughout
+		System.out.println("What's your name?");
+		String userName = scan.nextLine();
+		
+		//set up giant loop
 		boolean loop = true;
 		while (loop == true) {
-
+			
 			// get input from user
-			System.out.println("Please enter an integer between 1 and 100: ");
+			System.out.println(userName + ", please enter an integer between 1 and 100: ");
 			int userIn = scan.nextInt();
 			
 			// determine whether the integer is even or odd, and change outcome based on
 			// that
+			System.out.println(userName + ", here's the information you asked for:");
 			if (userIn % 2 != 0) {
 				if (userIn > 60) {
 					System.out.println(userIn + ", Odd and over 60");
@@ -30,21 +35,24 @@ public class Lab2 {
 					System.out.println(userIn + ", Even");
 				}
 			}
+			//continue asking the user for input until they give you a yes or no answer
 			boolean goodInput = false;
 			while (goodInput == false) {
 				System.out.println("Continue? (y / n)");
 				String isDone = scan.next();
 				char isDoneChar = isDone.charAt(0);
 				if (isDoneChar == 'y') {
+					//if the user says yes, break the y/n loop but go back to the beginning of the main loop
 					goodInput = true;
 				}
 				else if (isDoneChar == 'n') {
+					//if the user says no, break the y/n loop and the main loop and exit the program
 					goodInput = true;
 					loop = false;
-					System.out.println("See ya!");
+					System.out.println("Have a good day, " + userName + "!");
 				}
 				else {
-					System.out.println("Sorry, I don't understand that, please try again.");
+					System.out.println("Sorry, I don't understand that " + userName +  ", please try again.");
 				}
 			}
 		}
